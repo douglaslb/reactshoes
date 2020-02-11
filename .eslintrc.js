47076044681d53590bd9e24b5f7ab45e910a57a6
Douglas <doug.lb07@gmail.com>
2020-02-11 19:49:35 -0300

    Removing unused files
    Signed-off-by: Douglas <doug.lb07@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier/react',
+    'prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'semi': [2, 'never'],
+    'react/jsx-filename-extension': [
+      'warn',
+      {extensions: ['.jsx', '.js']}
+    ],
+    'import/prefer-default-export': 'off',
+    'react/prop-types': 0
+  },
+};
