540c52b1df8899e7b0aace99d7a90008cfcbfc08
Douglas <doug.lb07@gmail.com>
2020-02-13 23:19:51 -0300

    Adding style to cart page
    Signed-off-by: Douglas <doug.lb07@gmail.com>

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,68 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react'
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md'
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles'
 
 export default function Cart() {
-  return <h1>Carrinho</h1>
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QUANTIDADE</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-fila-disruptor-ii-premium-masculino/14/D29-4509-014/D29-4509-014_zoom1.jpg?ims=300x"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>TRIPLE S SNEAKER</strong>
+              <span>R$5.852,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline color="#7159c1" size={20} />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline color="#7159c1" size={20} />
+                </button>
+              </div>
+            </td>
+            <td>
+              <span>R$5.852,90</span>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar Pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 5.925,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  )
 }
