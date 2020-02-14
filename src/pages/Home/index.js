54328311b62effc4aeef181cad54895cb712cdbc
Douglas <doug.lb07@gmail.com>
2020-02-13 22:49:43 -0300

    Adding style to home
    Signed-off-by: Douglas <doug.lb07@gmail.com>

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,112 @@
 import React from 'react'
+import { MdAddShoppingCart } from 'react-icons/md'
 
-// import { Container } from './styles';
+import { ProductList } from './styles'
 
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-fila-disruptor-ii-premium-masculino/14/D29-4509-014/D29-4509-014_zoom1.jpg?ims=300x"
+          alt="Tênis"
+        />
+        <strong>TRIPLE S SNEAKER</strong>
+        <span>R$5.852,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-fila-disruptor-ii-premium-masculino/14/D29-4509-014/D29-4509-014_zoom1.jpg?ims=300x"
+          alt="Tênis"
+        />
+        <strong>TRIPLE S SNEAKER</strong>
+        <span>R$5.852,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-fila-disruptor-ii-premium-masculino/14/D29-4509-014/D29-4509-014_zoom1.jpg?ims=300x"
+          alt="Tênis"
+        />
+        <strong>TRIPLE S SNEAKER</strong>
+        <span>R$5.852,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-fila-disruptor-ii-premium-masculino/14/D29-4509-014/D29-4509-014_zoom1.jpg?ims=300x"
+          alt="Tênis"
+        />
+        <strong>TRIPLE S SNEAKER</strong>
+        <span>R$5.852,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-fila-disruptor-ii-premium-masculino/14/D29-4509-014/D29-4509-014_zoom1.jpg?ims=300x"
+          alt="Tênis"
+        />
+        <strong>TRIPLE S SNEAKER</strong>
+        <span>R$5.852,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-fila-disruptor-ii-premium-masculino/14/D29-4509-014/D29-4509-014_zoom1.jpg?ims=300x"
+          alt="Tênis"
+        />
+        <strong>TRIPLE S SNEAKER</strong>
+        <span>R$5.852,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  )
 }
