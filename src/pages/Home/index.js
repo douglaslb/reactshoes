b5cae1a7edea75da7698979e2fcf5dbe5217883b
Douglas <doug.lb07@gmail.com>
2020-02-15 01:21:18 -0300

    Fetching products
    Signed-off-by: Douglas <doug.lb07@gmail.com>

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,112 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { MdAddShoppingCart } from 'react-icons/md'
 
+import { api } from '../../services/api'
+import { formatPrice } from '../../util/format'
 import { ProductList } from './styles'
 
 export default function Home() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await api.get('products')
+
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormatted: formatPrice(product.price),
+      }))
+
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
+
   return (
     <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-fila-disruptor-ii-premium-masculino/14/D29-4509-014/D29-4509-014_zoom1.jpg?ims=300x"
-          alt="Tênis"
-        />
-        <strong>TRIPLE S SNEAKER</strong>
-        <span>R$5.852,90</span>
+      {products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" /> 3
+            </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-fila-disruptor-ii-premium-masculino/14/D29-4509-014/D29-4509-014_zoom1.jpg?ims=300x"
-          alt="Tênis"
-        />
-        <strong>TRIPLE S SNEAKER</strong>
-        <span>R$5.852,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-fila-disruptor-ii-premium-masculino/14/D29-4509-014/D29-4509-014_zoom1.jpg?ims=300x"
-          alt="Tênis"
-        />
-        <strong>TRIPLE S SNEAKER</strong>
-        <span>R$5.852,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-fila-disruptor-ii-premium-masculino/14/D29-4509-014/D29-4509-014_zoom1.jpg?ims=300x"
-          alt="Tênis"
-        />
-        <strong>TRIPLE S SNEAKER</strong>
-        <span>R$5.852,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-fila-disruptor-ii-premium-masculino/14/D29-4509-014/D29-4509-014_zoom1.jpg?ims=300x"
-          alt="Tênis"
-        />
-        <strong>TRIPLE S SNEAKER</strong>
-        <span>R$5.852,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-fila-disruptor-ii-premium-masculino/14/D29-4509-014/D29-4509-014_zoom1.jpg?ims=300x"
-          alt="Tênis"
-        />
-        <strong>TRIPLE S SNEAKER</strong>
-        <span>R$5.852,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   )
 }
