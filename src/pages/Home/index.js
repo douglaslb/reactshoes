3162ba8af681dc7d2798300fa53282f34e1888f6
Douglas <doug.lb07@gmail.com>
2020-02-17 15:58:16 -0300

    Creating cart reducer
    Signed-off-by: Douglas <doug.lb07@gmail.com>

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { MdAddShoppingCart } from 'react-icons/md'
+import { connect } from 'react-redux'
 
 import { api } from '../../services/api'
 import { formatPrice } from '../../util/format'
 import { ProductList } from './styles'
 
-export default function Home() {
+function Home({ dispatch }) {
   const [products, setProducts] = useState([])
 
   useEffect(() => {
@@ -22,6 +23,13 @@ export default function Home() {
     fetchProducts()
   }, [])
 
+  const handleAddProduct = product => {
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    })
+  }
+
   return (
     <ProductList>
       {products.map(product => (
@@ -30,7 +38,7 @@ export default function Home() {
           <strong>{product.title}</strong>
           <span>{product.priceFormatted}</span>
 
-          <button type="button">
+          <button type="button" onClick={() => handleAddProduct(product)}>
             <div>
               <MdAddShoppingCart size={16} color="#fff" /> 3
             </div>
@@ -42,3 +50,5 @@ export default function Home() {
     </ProductList>
   )
 }
+
+export default connect()(Home)
