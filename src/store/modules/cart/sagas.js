27e3093be0c7b17b79137117da2b4d0fa6c5db77
Douglas Lima <doug.lb07@gmail.com>
2020-02-26 16:33:12 -0300

    Handling product stock

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,12 +1,37 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects'
+import { call, select, put, all, takeLatest } from 'redux-saga/effects'
 
 import { api } from '../../../services/api'
-import { addtoCartSuccess } from './actions'
+import { formatPrice } from '../../../util/format'
+import { addtoCartSuccess, updateAmount } from './actions'
 
 function* addToCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`)
+  const productExists = yield select(state => state.cart.find(p => p.id === id))
 
-  yield put(addtoCartSuccess(response.data))
+  const stock = yield call(api.get, `/stock/${id}`)
+
+  const stockAmount = stock.data.amount
+  const amount = productExists ? productExists.amount : 0
+
+  const currentAmount = amount + 1
+
+  if (currentAmount > stockAmount) {
+    console.tron.warn('ERRO')
+    return
+  }
+
+  if (productExists) {
+    yield put(updateAmount(id, currentAmount))
+  } else {
+    const response = yield call(api.get, `/products/${id}`)
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    }
+
+    yield put(addtoCartSuccess(data))
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)])
