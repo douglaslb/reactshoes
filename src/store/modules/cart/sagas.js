1e500cb81f27f95a4d8b3ee48c247b6ef0de57af
Douglas Lima <doug.lb07@gmail.com>
2020-02-26 17:10:06 -0300

    Handling quantity of products in stock

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -3,7 +3,7 @@ import { toast } from 'react-toastify'
 
 import { api } from '../../../services/api'
 import { formatPrice } from '../../../util/format'
-import { addtoCartSuccess, updateAmount } from './actions'
+import { addtoCartSuccess, updateAmountSuccess } from './actions'
 
 function* addToCart({ id }) {
   const productExists = yield select(state => state.cart.find(p => p.id === id))
@@ -21,7 +21,7 @@ function* addToCart({ id }) {
   }
 
   if (productExists) {
-    yield put(updateAmount(id, currentAmount))
+    yield put(updateAmountSuccess(id, currentAmount))
   } else {
     const response = yield call(api.get, `/products/${id}`)
 
@@ -35,4 +35,21 @@ function* addToCart({ id }) {
   }
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)])
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return
+
+  const stock = yield call(api.get, `/stock/${id}`)
+  const stockAmount = stock.data.amount
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitada fora do estoque')
+    return
+  }
+
+  yield put(updateAmountSuccess(id, amount))
+}
+
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+])
